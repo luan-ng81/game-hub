ab85f980ccc8299853bbf76e445c5ff567d10026
Luan Nguyen <luannguyen8118@gmail.com>
2023-08-02 16:08:07 -0700

    Build expandable text

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,5 +1,5 @@
 import { Heading, Spinner, Text } from "@chakra-ui/react";
-import React from "react";
+import ExpandableText from "../components/ExpandableText";
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
 
@@ -14,9 +14,9 @@ const GameDetailPage = () => {
   return (
     <>
       <Heading>{game.name}</Heading>
-      <Text>{game.description_raw}</Text>
+      <ExpandableText>{game.description_raw}</ExpandableText>
     </>
   );
 };
 
-export default GameDetailPage
+export default GameDetailPage;
